83922091d7ec9f9fc742b0cd60b7540f8a9a7bf2
VladKhurs <namesurmane777@gmail.com>
2024-08-18 00:37:52 +0300

    feat: add product api

--- server/controllers/deviceController.js
+++ server/controllers/deviceController.js
@@ -1,6 +1,18 @@
+const uuid = require('uuid')
+const path = require('path')
+const {Device} = require('../modules/modules')
+const ApiError = require('../error/ApiError')
+
 class DeviceController {
-  async create (req, res) {
-    
+  async create (req, res, next) {
+    try {
+      const {name, price, brandId, typeId, img, info} = req.body
+
+      const device = await Device.create({name, price, brandId, typeId, img})
+      return res.json(device)
+    } catch (e) {
+      next(ApiError.badRequest(e.message))
+    }
   }
 
   async getAll (req, res) {
--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const express = require('express')
 const sequelize = require('./db')
 const modules = require('./modules/modules')
 const cors = require('cors')
+const fileUpload = require('express-fileupload')
 const router = require('./routes/index')
 const errorHandler = require('./middleware/ErrorHandlingMiddlewarre')
 
@@ -12,6 +13,7 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 app.use('/api', router)
+app.use(fileUpload({}))
 app.use(errorHandler)
 
 app.get('/', (req, res) => {
